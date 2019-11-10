2dfc205670daf9b8f2a5c499ea3e494698bbaa44
Vicky Law <vicky.law1217@gmail.com>
2019-11-10 15:53:00 -0500

    drop down rooms created

--- client/src/components/RoomsDropDown/index.js
+++ client/src/components/RoomsDropDown/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class PeopleDropDown extends React.Component {
+class RoomsDropDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: "" };
@@ -22,33 +22,23 @@ class PeopleDropDown extends React.Component {
     return (
       <div class="form-group" onSubmit={this.handleSubmit}>
         {/* <form onSubmit={this.handleSubmit}> */}
-        <label for="FormControlSelect2">People </label>
+        <label for="FormControlSelect3">Number of Room</label>
         <select
           class="form-control"
-          id="FormControlSelect2"
+          id="FormControlSelect3"
           value={this.state.value}
           onChange={this.handleChange}
         >
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-          <option>6</option>
-          <option>7</option>
-          <option>8</option>
-          <option>9</option>
-          <option>10</option>
-          <option>11</option>
-          <option>12</option>
-          <option>13</option>
-          <option>14</option>
-          <option>15</option>
-          <option>16</option>
-          <option>17</option>
-          <option>18</option>
-          <option>19</option>
-          <option>20</option>
+          <option>One</option>
+          <option>Two</option>
+          <option>Three</option>
+          <option>Four</option>
+          <option>Five</option>
+          <option>Six</option>
+          <option>Seven</option>
+          <option>Eight</option>
+          <option>Nine</option>
+          
         </select>
 
         {/* <input type="submit" value="Search" /> */}
@@ -57,4 +47,4 @@ class PeopleDropDown extends React.Component {
   }
 }
 
-export default PeopleDropDown;
+export default RoomsDropDown;
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -5,17 +5,26 @@ import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import LocationDropDown from "../components/LocationDropDown";
 import PeopleDropDown from "../components/PeopleDropDown";
+import RoomsDropDown from "../components/RoomsDropDown";
 
 //import { List, ListItem } from "../components/List";
 //import { Input, TextArea, FormBtn } from "../components/Form";
 
 function MainPage() {
   return (
-    <Container fluid>
+    <Container>
       <Row>
         <Col size="md-12">
           <LocationDropDown />
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-3">
           <PeopleDropDown />
+        </Col>
+
+        <Col size="md-3">
+          <RoomsDropDown />
         </Col>
       </Row>
     </Container>
