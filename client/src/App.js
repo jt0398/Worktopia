78b96eaddb4d309fd73396f7a52d7bd9c2503936
Vicky Law <vicky.law1217@gmail.com>
2019-11-11 12:39:59 -0500

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,20 @@ import Login from "./pages/Login";
 import WorkSpaceDetail from "./pages/WorkSpaceDetail";
 import SearchResults from "./pages/SearchResults";
 import NoMatch from "./pages/NoMatch";
+//import Jumbotron from "./components/Jumbotron";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Nav from "./components/Nav";
 import MainPage from "./pages/MainPage";
-
 
 function App() {
   return (
     <Router>
       <div>
+        {/* <Jumbotron /> */}
         <Nav />
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/main" component={MainPage} />
-         
           <Route exact path="/workspacedetail" component={WorkSpaceDetail} />
           <Route exact path="/searchresults" component={SearchResults} />
           <Route component={NoMatch} />
