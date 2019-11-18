5e964625481982c7812ef98ef88e7646616b8a54
Sachin796 <sachinjaveri.2010@gmail.com>
2019-11-17 21:59:31 -0500

    Autocomplete for search page

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -4,6 +4,12 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { SingleDatePicker } from "react-dates";
 import "./style.css";
+import PlacesAutocomplete from "react-places-autocomplete";
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng
+} from "react-places-autocomplete";
 
 class Search extends Component {
   constructor(props) {
@@ -36,7 +42,19 @@ class Search extends Component {
 
   state = {
     focusedCheckIn: null,
-    focusedCheckOut: null
+    focusedCheckOut: null,
+    address: ""
+  };
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
   };
 
   render() {
@@ -50,13 +68,56 @@ class Search extends Component {
                   <i class="fas fa-thumbtack"></i> Locations
                 </strong>
               </Form.Label>
-              <Form.Control
+              {/* <Form.Control
                 type="text"
                 required
                 onChange={this.props.onChange}
                 value={this.props.location}
                 name="location"
-              />
+              /> */}
+              <PlacesAutocomplete
+                value={this.state.address}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
+              >
+                {({
+                  getInputProps,
+                  suggestions,
+                  getSuggestionItemProps,
+                  loading
+                }) => (
+                  <div>
+                    <input
+                      {...getInputProps({
+                        placeholder: "Search Places ...",
+                        className: "location-search-input"
+                      })}
+                    />
+                    <div className="autocomplete-dropdown-container">
+                      {loading && <div>Loading...</div>}
+                      {suggestions.map(suggestion => {
+                        const className = suggestion.active
+                          ? "suggestion-item--active"
+                          : "suggestion-item";
+                        // inline style for demonstration purpose
+                        const style = suggestion.active
+                          ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                          : { backgroundColor: "#ffffff", cursor: "pointer" };
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, {
+                              className,
+                              style
+                            })}
+                          >
+                            <span>{suggestion.description}</span>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
+              </PlacesAutocomplete>
             </Form.Group>
           </Form.Row>
           <Form.Row>
