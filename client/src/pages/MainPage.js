be7bc22b6d235c8056dd15baa89d61ede5c000fb
Sachin796 <sachinjaveri.2010@gmail.com>
2019-11-18 19:16:11 -0500

    Final main page commit (#79)

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,91 +3,144 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+//import Grid from "../components/Grid";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
-import AboutUs from "../components/AboutUsCard";
-import Testimonials from "../components/Testimonails";
+import { Animated } from "react-animated-css";
 import "./css/MainPage.css";
 
 class MainPage extends Component {
-  data = [
-    {
-      upperText: "Flexible",
-      image_path: "images/Flexible.png",
-      lowerText: "Any dates you can choose"
-    },
-    {
-      upperText: "Flexible",
-      image_path: "images/Flexible.png",
-      lowerText: "Any dates you can choose"
-    },
-    {
-      upperText: "Flexible",
-      image_path: "images/Flexible.png",
-      lowerText: "Any dates you can choose"
-    }
-  ];
-
   render() {
     return (
       <Container fluid>
-        <div className="demo">
-          <div id="text1" className="col-md-5 ">
-            <h1>Probably the best Co-Working Space in the city</h1>
-            <div class="btn btn-primary btn1">Click Here</div>
+        <div className="boxShadow">
+          <div className="demo text-center">
+            <Animated
+              animationIn="lightSpeedIn"
+              animationInDelay={1000}
+              animationOut="fadeOut"
+              isVisible={true}
+            >
+              <h1 className="intro">Welcome to Worktopia</h1>
+            </Animated>
           </div>
         </div>
-        <div className="contentOnScroll">
-          <Row>
-            <Col size="md-12">
-              <div className="searchBox">
-                <Search />
-              </div>
+
+        <Row>
+          <Col size="md-12">
+            <div className="boxShadow">
+              <Search />
+            </div>
+          </Col>
+        </Row>
+
+        <div className="boxShadow">
+          <div className="parallaxImg"></div>
+        </div>
+
+        <div className="textNimg">
+          <Row className="text-center">
+            <Col md={4} id="textimg">
+              <h1>Flexible</h1>
+              <Animated
+                animationIn="rotateIn"
+                animationInDelay={3000}
+                animationOut="fadeOut"
+                isVisible={true}
+              >
+                <img
+                  src="images/Flexible.png"
+                  width={90}
+                  height={120}
+                  alt="flexibility"
+                ></img>
+              </Animated>
+              <br></br>
+              <br></br>
+              <h5>Any dates you can choose</h5>
+            </Col>
+            <Col md={4} id="textimg">
+              <h1>Convenient</h1>
+              <Animated
+                animationIn="rotateIn"
+                animationInDelay={4000}
+                animationOut="fadeOut"
+                isVisible={true}
+              >
+                <img
+                  src="images/Convenient.png"
+                  width={90}
+                  height={120}
+                  alt="flexibility"
+                ></img>
+              </Animated>
+              <br></br>
+              <br></br>
+              <h5>Anywhere you can choose</h5>
+            </Col>
+            <Col md={4} id="textimg">
+              <h1>Comfortable</h1>
+              <Animated
+                animationIn="rotateIn"
+                animationInDelay={5000}
+                animationOut="fadeOut"
+                isVisible={true}
+              >
+                <img
+                  src="images/Comfortable.png"
+                  width={80}
+                  height={120}
+                  alt="flexibility"
+                ></img>
+              </Animated>
+              <br></br>
+              <br></br>
+              <h5>Any styles you can choose</h5>
             </Col>
           </Row>
         </div>
-        <div className="searchBars"></div>
-        <Row>
-          {this.data.map(element => {
-            return (
-              <Col md="4" sm="12">
-                <AboutUs {...element}></AboutUs>
-              </Col>
-            );
-          })}
-          ;
-        </Row>
-        <br></br>
-        <Row className="text-center"></Row>
-        <div className="searchBars"></div>
+
+        <div className="boxShadow">
+          <div className="parallaxImg"></div>
+        </div>
+
         <div className="textNimg">
           <p>
             <h1>Here well include why worktopia?</h1>
-            Where work meets play Toronto is known for its ‘work hard, play
-            hard’ attitude, but a new cache of coworking spaces are bringing
-            these spheres together under one roof. Creatives in Canada’s biggest
-            metropolis – one of our ‘cities to watch in 2018‘ – are ditching the
+            Where work meets play Toronto is known for its 'work hard, play
+            hard' attitude, but a new cache of coworking spaces are bringing
+            these spheres together under one roof. Creatives in Canada's biggest
+            metropolis – one of our 'cities to watch in 2018' – are ditching the
             sterility of a serviced office and seeking out hybrid hubs that
             cater to their business, as well as their personal interests. Sited
             in former factories and warehouses, these professional playgrounds
             offer wellness classes, networking and social events, from concerts
-            to pot-lucks. Here are seven of Toronto’s best coworking spaces.
+            to pot-lucks. Here are seven of Toronto's best coworking spaces.
           </p>
         </div>
-        <div className="searchBars"></div>
-        <div className="textNimg">
-          <Row>
-            {this.data.map(element => {
-              return (
-                <Col md="4" sm="12">
-                  <Testimonials {...element}></Testimonials>
-                </Col>
-              );
-            })}
-            ;
-          </Row>
+
+        <div className="boxShadow">
+          <div className="parallaxImg"></div>
         </div>
-        <div className="searchBars"></div>'
+
+        <div className="textNimg">
+          <p>
+            <h1>Here well include testimonials</h1>
+            Where work meets play Toronto is known for its 'work hard, play
+            hard' attitude, but a new cache of coworking spaces are bringing
+            these spheres together under one roof. Creatives in Canada's biggest
+            metropolis – one of our 'cities to watch in 2018' – are ditching the
+            sterility of a serviced office and seeking out hybrid hubs that
+            cater to their business, as well as their personal interests. Sited
+            in former factories and warehouses, these professional playgrounds
+            offer wellness classes, networking and social events, from concerts
+            to pot-lucks. Here are seven of Toronto's best coworking spaces.
+          </p>
+        </div>
+
+        <div className="boxShadow">
+          <div className="parallaxImg"></div>
+        </div>
         <Footer />
       </Container>
     );
