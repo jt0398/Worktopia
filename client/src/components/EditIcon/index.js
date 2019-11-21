e3108a3217ace7940d62c23230db55bd577422be
Vicky Law <vicky.law1217@gmail.com>
2019-11-21 16:49:25 -0500

    owner location style updated

--- client/src/components/EditIcon/index.js
+++ client/src/components/EditIcon/index.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import { faEdit} from "@fortawesome/free-solid-svg-icons";
-export function EditIcon({id}) {
-    return (
-        <Link to={`/owner/editlocation/${id}`} data-toggle="tooltip" data-placement="top" title="Edit Location"><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></Link>
-    )
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
+export function EditIcon({ id }) {
+  return (
+    <Link
+      to={`/owner/editlocation/${id}`}
+      data-toggle="tooltip"
+      data-placement="top"
+      title="Edit Location"
+    >
+      <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+    </Link>
+  );
 }
