24a806aeeeb8f78ce81ce2a8cba049c26c19a6b3
Sachin796 <sachinjaveri.2010@gmail.com>
2019-11-21 18:34:20 -0500

    Navbar working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,23 +2,89 @@ import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import Modal from "../Modal";
 
+const noLOGIN = [
+  ["HOME", "/"],
+  ["LOGIN", "/login"]
+];
+
+const userLOGIN = [
+  ["HOME", "/"],
+  ["MY BOOKINGS", "/booking/user"],
+  ["SIGN OUT", "/logout"]
+];
+
+const ownerLOGIN = [
+  ["HOME", "/"],
+  ["ADD WORKSPACE", "/"],
+  ["ADD LOCATION", "/location"],
+  ["MY BOOKINGS", "/booking/owner"],
+  ["SIGN OUT", "/logout"]
+];
+
 export class Header extends Component {
+  state = {
+    navItems: []
+  };
+
+  componentDidMount() {
+    localStorage.setItem("UserId", 2);
+    localStorage.setItem("UserRole", 2);
+
+    //1 Owner, 2 Customer
+
+    //Owner
+    if (parseInt(localStorage.getItem("UserRole")) === 1) {
+      this.setState({
+        navItems: ownerLOGIN
+      });
+    }
+    //Customer
+    else if (parseInt(localStorage.getItem("UserRole")) === 2) {
+      this.setState({
+        navItems: userLOGIN
+      });
+    }
+    //Anonymous
+    else if (!parseInt(localStorage.getItem("UserRole"))) {
+      this.setState({
+        navItems: noLOGIN
+      });
+    }
+  }
+
   render() {
+    const linktopage = link => {
+      alert("Will go to  " + link);
+      window.location.pathname = link;
+    };
     return (
       <>
         <Navbar bg="dark" expand="lg">
           <Navbar.Brand className="text-warning" href="/main">
             Worktopia
           </Navbar.Brand>
-          <Nav defaultActiveKey="/main" as="ul" class="nav justify-content-end">
-            <Nav.Item as="li">
-              <Nav.Link className="text-light" href="/main">
-                Home
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Modal />
-            </Nav.Item>
+          <Nav
+            defaultActiveKey="/main"
+            as="ul"
+            class="nav ml-auto"
+            onSelect={linktopage}
+          >
+            {this.state.navItems.map(items => {
+              return (
+                <Nav.Item
+                  id="n"
+                  as="li"
+                  href={items[1]}
+                  className="mr-3"
+                  style={{ color: "white" }}
+                >
+                  {console.log(window.location)}
+                  <Nav.Link className="text-light" eventKey={items[1]}>
+                    {items[0]}
+                  </Nav.Link>
+                </Nav.Item>
+              );
+            })}
           </Nav>
         </Navbar>
       </>
