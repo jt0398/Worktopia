efcc2c45fcb655337fda595413b552466b702fc0
Unni Ramachandran <unnipbvr@gmail.com>
2019-11-21 20:00:28 -0500

    Google sign in integration

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import API from "../utils/API";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { GoogleLogin } from "react-google-login";
 
 class Login extends Component {
   state = {
@@ -29,11 +31,23 @@ class Login extends Component {
       API.checkLogin(this.state)
         .then(res => {
           console.log("API Response ", res.data);
+          localStorage.setItem("UserId", res.data.id);
+          localStorage.setItem("UserRole", res.data.UserRoleId);
+          if (res.data.UserRoleId === 1) {
+            console.log("Owner logged in");
+            return <Redirect to="/owner/{res.data.id}" />;
+          } else {
+            console.log("User logged in");
+            return <Redirect to="/" />;
+          }
         })
         .catch(err => console.log(err));
     }
   };
 
+  responseGoogle = response => {
+    console.log(response);
+  };
   render() {
     return (
       <Container fluid>
@@ -69,6 +83,17 @@ class Login extends Component {
             </form>
           </Col>
         </Row>
+        <br></br>
+        <br></br>
+        <GoogleLogin
+          // clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+        ,
       </Container>
     );
   }
