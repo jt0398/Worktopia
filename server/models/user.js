6bb38080bdc01e16aaae7e7614fd52e228dbfc0a
unnikrishnan-r <35500594+unnikrishnan-r@users.noreply.github.com>
2019-11-23 11:48:32 -0500

    Duplicate username issue (#141)
    * Adding unique index on username on User table
    
    * Removed the google login button
    
    * Redirecting to main page on succesfull signup, showing modal for errors

--- server/models/user.js
+++ server/models/user.js
@@ -1,38 +1,50 @@
 var bcrypt = require("bcryptjs");
 
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    username: {
-      type: DataTypes.STRING(20),
-      allowNull: false
+  var User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+      },
+      addr1: {
+        type: DataTypes.STRING(100)
+      },
+      addr2: {
+        type: DataTypes.STRING(100)
+      },
+      city: {
+        type: DataTypes.STRING(50)
+      },
+      province: {
+        type: DataTypes.STRING(50)
+      },
+      postal_code: {
+        type: DataTypes.STRING(20)
+      },
+      phone_no: {
+        type: DataTypes.STRING(50)
+      }
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(50),
-      allowNull: true
-    },
-    addr1: {
-      type: DataTypes.STRING(100)
-    },
-    addr2: {
-      type: DataTypes.STRING(100)
-    },
-    city: {
-      type: DataTypes.STRING(50)
-    },
-    province: {
-      type: DataTypes.STRING(50)
-    },
-    postal_code: {
-      type: DataTypes.STRING(20)
-    },
-    phone_no: {
-      type: DataTypes.STRING(50)
+    {
+      indexes: [
+        // Create a unique index on username
+        {
+          unique: true,
+          fields: ["username"]
+        }
+      ]
     }
-  });
+  );
 
   User.associate = function(models) {
     models.User.belongsTo(models.UserRole, {
