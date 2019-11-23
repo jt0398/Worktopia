0dd1d3ad1ea60cf037d878e90697ff3fd893e258
Sachin796 <sachinjaveri.2010@gmail.com>
2019-11-23 13:15:39 -0500

    Latest Login Page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,9 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { GoogleLogin } from "react-google-login";
+import "./css/login.css";
+import Card from "react-bootstrap/Card";
 
 class Login extends Component {
   state = {
@@ -54,39 +57,70 @@ class Login extends Component {
     }
 
     return (
-      <Container fluid>
+      <Container fluid style={{ height: "100vh" }}>
         <br></br>
-        {/* <br></br> */}
-        <Row>
-          <Col size="md-6">
-            <form>
-              <Form.Control
-                value={this.state.username}
-                onChange={this.handleInputChange}
-                name="username"
-                placeholder="Enter your username"
-              />
-              <br></br>
+        <Card
+          className="mt-5"
+          style={{
+            width: "22rem",
+            height: "70vh",
+            margin: "auto"
+          }}
+        >
+          <Card.Img variant="top  " src="/images/loginimg.jpg" />
+          <Card.ImgOverlay>
+            <Card.Text>
+              <Col className="text-center">
+                <h3 style={{ "line-height": "100px", color: "#7fff00" }}>
+                  LOGIN
+                </h3>
+              </Col>
+            </Card.Text>
+          </Card.ImgOverlay>
+          <Card.Body>
+            <Row>
+              <Col md={12} sm={6}>
+                <form>
+                  <Form.Control
+                    value={this.state.username}
+                    onChange={this.handleInputChange}
+                    name="username"
+                    placeholder="Enter your username"
+                  />
+                  <br></br>
+                  <Form.Control
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
+                    name="password"
+                    type="password"
+                    placeholder="Enter your password"
+                  />
+                  <br></br>
+                </form>
+              </Col>
+            </Row>
 
-              <Form.Control
-                value={this.state.password}
-                onChange={this.handleInputChange}
-                name="password"
-                type="password"
-                placeholder="Enter your password"
-              />
-              <br></br>
-
+            <div className="text-center">
               <Button
-                className="btn btn-info"
+                className="btn btn-info "
                 disabled={this.validateFormCompletion()}
                 onClick={this.handleFormSubmit}
               >
                 Log In
               </Button>
-            </form>
-          </Col>
-        </Row>
+            </div>
+
+            <Row>
+              <Col className="text-center" md={12} sm={6}>
+                <br></br>
+                <h5>
+                  Create an Account...?<br></br>
+                  <br></br> <a href="/signup">SIGN UP </a>
+                </h5>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
       </Container>
     );
   }
