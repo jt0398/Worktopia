fc8ef05638f60c069c64b41771f097d06134d4c2
Judy <judys717@hotmail.com>
2019-11-27 00:39:47 -0500

    fix hide or show of pay with card depending on availability

--- client/src/pages/BookWorkspace.js
+++ client/src/pages/BookWorkspace.js
@@ -73,6 +73,8 @@ class BookWorkspace extends Component {
     });
 
     localStorage.setItem("checkinDate", date.format("MM/DD/YYYY"));
+
+    this.checkAvailability();
   };
 
   //Update Check Out state
@@ -82,6 +84,8 @@ class BookWorkspace extends Component {
     });
 
     localStorage.setItem("checkoutDate", date.format("MM/DD/YYYY"));
+
+    this.checkAvailability();
   };
 
   componentDidMount() {
@@ -122,6 +126,7 @@ class BookWorkspace extends Component {
 
   checkAvailability() {
     //Create booking in the database
+    //I use localStorage because state change is async
     const booking = {
       start_date: moment(localStorage.getItem("checkinDate")).format(
         "MM/DD/YYYY"
